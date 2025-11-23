3f804d60e148b869a8f3fc7e24a9dde7e9f6c86e
Attula15 <herincs.bence.kdg@gmail.com>
2025-11-23 19:20:03 +0100

    admin endpoint added

--- src/main/java/com/bherincs/forumpractice/controllers/GreetingController.java
+++ src/main/java/com/bherincs/forumpractice/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.bherincs.forumpractice.controllers;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(){
         return "Hello world";
+    }
+
+    @GetMapping("admin")
+    public String admin(){
+        return  "admin";
     }
 }
