fa7ba738becc60d50ce8a2caf516378196750a75
Attula15 <herincsb@edu.bme.hu>
2025-11-23 11:17:07 +0100

    Init from Spring Initializr

---
+++ src/main/java/com/bherincs/forumpractice/ForumpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.bherincs.forumpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bherincs/forumpractice/ForumpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bherincs.forumpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
